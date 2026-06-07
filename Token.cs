4a059e03d3740e1672dfd04fd18f237b259481a9
Bones <terraw1999@proton.me>
2026-06-07 16:07:53 -0600

    Add: Token class and TokenType enum

---
+++ Token.cs
@@ -0,0 +1,36 @@
+namespace DotLox;
+
+class Token {
+	readonly TokenType 	type;
+	readonly string 	lexeme;
+	readonly Object 	literal;
+	readonly int 		line;
+
+	Token(TokenType type, string lexeme, Object literal, int line) {
+		this.type 		= type;
+		this.lexeme 	= lexeme;
+		this.literal 	= literal;
+		this.line		= line;
+	}
+
+	public string toString() {
+		return $"type {lexeme} {literal}";
+	}
+}
+
+enum TokenType {
+	// Single character tokens
+	LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+	// Conditional tokens
+	BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+
+	// Literals
+	IDENT, STRING, NUM,
+
+	// Keywords
+	AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR, PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+	// End of file
+	EOF
+}
