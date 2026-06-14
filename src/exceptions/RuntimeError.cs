8952f3acd6c27d108dc917fb5ca7b439149548ef
Bones <terraw1999@proton.me>
2026-06-13 20:28:24 -0600

    TODO: Make token field private and add getter with null handling

--- src/exceptions/RuntimeError.cs
+++ src/exceptions/RuntimeError.cs
@@ -1,6 +1,7 @@
 namespace DotLox.Exception;
 
 public class RuntimeError : System.Exception {
+	// TODO: Make private and use getter with null handling
 	public readonly Token? token;
 
 	public RuntimeError(Token? token, string message) : base(message) {
