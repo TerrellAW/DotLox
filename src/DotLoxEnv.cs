43b3864ed513beac3c46e014959ec0399bb55008
Bones <terraw1999@proton.me>
2026-06-16 13:27:33 -0600

    Add: Nesting scope implementation, comments

--- src/DotLoxEnv.cs
+++ src/DotLoxEnv.cs
@@ -2,39 +2,57 @@ using DotLox.Exception;
 
 namespace DotLox;
 
+// Environment for variables, represents global and local scopes
 public class DotLoxEnv {
+	// Scope that encloses this one, null for global
 	internal readonly DotLoxEnv? enclosing;
+	// Variables are stored as key-value pairs
 	private readonly Dictionary<string, object?> values = new Dictionary<string, object?>();
 
+	// Getter for enclosing scope
 	public DotLoxEnv? getEnclosing() {
 		return enclosing;
 	}
 	
+	// Argument-less constructor for global env
 	public DotLoxEnv() {
 		this.enclosing = null;
 	}
 
+	// Local env nested in another env
 	public DotLoxEnv(DotLoxEnv enclosing) {
 		this.enclosing = enclosing;
 	}
 
+	// Retrieve variable value from name
 	public object? Get(Token name) {
 		if (values.ContainsKey(name.getLexeme())) {
 			return values[name.getLexeme()];
 		}
 
+		// If variable isn't found in scope, look up
+		if (enclosing != null) return enclosing.Get(name);
+
 		throw new RuntimeError(name, $"Undefined variable '{name.getLexeme()}'.");
 	}
 
+	// Assign a value to a variable
 	public void Assign(Token name, Object value) {
 		if (values.ContainsKey(name.getLexeme())) {
 			values[name.getLexeme()] = value;
 			return;
 		}
 
+		// If variable not declared in scope, look up
+		if (enclosing != null) {
+			enclosing.Assign(name, value);
+			return;
+		}
+
 		throw new RuntimeError(name, $"Undefined variable '{name.getLexeme()}'.");
 	}
 
+	// Define a variable
 	public void Define(string name, object? value) {
 		values.Add(name, value);
 	}
