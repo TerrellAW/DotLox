2760e947de36061147396a9119804f9fa6097724
Bones <terraw1999@proton.me>
2026-06-25 21:39:08 -0600

    Fix: Missing getter

--- src/Return.cs
+++ src/Return.cs
@@ -4,6 +4,10 @@ namespace DotLox;
 public class Return : System.Exception {
 	private readonly Object? value;
 
+	public Object? getValue() {
+		return value;
+	}
+
 	public Return(Object? value) : base(null, null) {
 		this.value = value;
 	}
